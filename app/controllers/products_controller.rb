07db91a03bf73ad7dbd182c682829948b69c8b60
= <rtyenriques@gmail.com>
2021-10-07 16:54:19 -0700

    commit tree dfb9b55a3bb33207682a835b16b55c89589838a6

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+    def index
+        
+        @cart = cart
+
+      end
+    
+      def add
+        cart << product_params
+        redirect_to :products
+      end
+    
+      private
+    
+      def product_params
+        params.require(:product)
+      end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'products#index', as: :products
+
+  post '/add', to: 'products#add', as: :add_product
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
